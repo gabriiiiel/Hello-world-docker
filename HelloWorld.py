6700852e318559f731f2096f9083463a04dacc1f
Gabriel69 <gabifranklin2003@gmail.com>
2023-11-02 10:14:45 +0530

    First change

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'This statement is a change made in my project!!'
 
 if __name__ == '__main__':
     app.run()
